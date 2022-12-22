37a36d1668d5744ffc9e2b4c2914fb37401fec0a
JeongMin-98 <jeongmin981@gmail.com>
2022-12-23 08:36:19 +0900

    add week5 problems

---
+++ Baekjoon/week5/쿼드트리.py
@@ -0,0 +1,57 @@
+"""
+
+    Baekjoon: https://www.acmicpc.net/problem/1992
+
+    쿼드 트리는 흑백영상을 압축하여 표현하는 데이터 구조
+
+    흰점은 0
+    검은점은 1
+
+    영상의 왼쪽 위, 오른쪽 위, 왼쪽 아래, 오른쪽 아래로 나눠 압축 한다.
+
+"""
+import math
+import sys
+
+
+def split(array, size, x0, x1, y0, y1):
+    """ array => 영상의 데이터 배열, size는 영상의 크기 ex) 8 = 2**3 size=3 """
+    global area
+    if math.log2(size) > 1:
+        size = size // 2
+        x_mid = x0 + size
+        y_mid = y0 + size
+
+        lu = split(array, size, x0, x_mid - 1, y0, y_mid - 1)  # 왼쪽 위
+        ru = split(array, size, x0, x_mid - 1, y_mid, y1)  # 오른쪽 위
+        ld = split(array, size, x_mid, x1, y0, y_mid - 1)  # 왼쪽 아래
+        rd = split(array, size, x_mid, x1, y_mid, y1)  # 오른쪽 아래
+
+    else:
+        area_value = set([array[x0][y0], array[x0][y1], array[x1][y0], array[x1][y1]])
+
+        if len(area_value) == 1:
+            area_value = list(area_value)
+            return str(area_value[0])
+        else:
+            return [array[x0][y0], array[x0][y1], array[x1][y0], array[x1][y1]]
+    if lu is None and ru is None and ld is None and rd is None:
+        return
+    area.append([lu, ru, ld, rd])
+    return
+
+
+def merge(splited_area):
+    """ 나눠진 영상의 영역에서 겹치는 영상부분을 합치는 함수 """
+
+    for i in range(len(splited_area)):
+
+
+if __name__ == '__main__':
+    N = int(input())
+    video = [list(map(int, input())) for _ in range(N)]
+
+    area = []
+    # N = math.log2(N)
+    split(video, N, 0, N - 1, 0, N - 1)
+    print(area)
